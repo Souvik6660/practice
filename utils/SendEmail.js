a5dbbb6be7cd8cfab41ab3cda135c287510e9560
Souvik6660 <146552354+Souvik6660@users.noreply.github.com>
2024-09-02 00:09:59 +0530

    Update SendEmail.js

--- utils/SendEmail.js
+++ utils/SendEmail.js
@@ -1,10 +1,4 @@
 import nodemailer from 'nodemailer';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-// Get the current directory of the module
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const SendEmail = async function (email, subject, message) {
   // Create reusable transporter object using the default SMTP transport
@@ -18,23 +12,13 @@ const SendEmail = async function (email, subject, message) {
     },
   });
 
-  // Resolve the correct path to the attachment
-  const attachmentPath = path.resolve(__dirname, 'models', 'sagar.jpeg');
-
   // Send mail with defined transport object
   try {
     await transporter.sendMail({
       from: `SAGAR ACADEMY <${process.env.SMTP_FROM_EMAIL}>`, // sender address
       to: email, // recipient address
       subject: subject, // Subject line
-      html: `${message} <br><img src="cid:sagar@108gmail.com" style="width: 200px;" />`, // HTML body with the embedded image
-      attachments: [
-        {
-          filename: 'sagar.jpeg',
-          path: attachmentPath,
-          cid: 'sagar@108gmail.com' // same CID as in the HTML img src
-        }
-      ]
+      html: message // HTML body
     });
     console.log('Email sent successfully');
   } catch (error) {
